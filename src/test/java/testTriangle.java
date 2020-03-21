092c2e57c9f0c69363bef348ba36de5d8c1b32b2
Sofia Gattucci <sofia.gattucci@studio.unibo.it>
2020-03-21 17:13:23 +0100

    Assignment3

--- src/test/java/testTriangle.java
+++ src/test/java/testTriangle.java
@@ -1,99 +1,55 @@
 import it.unimi.di.vec.ass1.TriangleImpl;
-import static org.junit.Assert.*;
-import org.junit.Before;
-import org.junit.Test;
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.Rule;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.PrintStream;
-import java.util.InputMismatchException;
 
 public class testTriangle{
     ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
     InputStream backupInputStream = System.in;
 
-    @Before
-    public void setUp(){
+    @BeforeEach
+    public void initialConfiguration() {
         System.setOut(new PrintStream(outputStream));
     }
 
-    @Test
-    public void createObject(){
-        setInputNumber("8 5 5");
-        TriangleImpl tri = new TriangleImpl();
-        assertNotNull(tri);
+
+    @ParameterizedTest
+    @ValueSource(strings = {"3 5 5",
+            "3 5 3",
+            "3 3 5"
+    })
+    public void isIsosceles(String value){
+        TriangleImpl tri = createTriangle(value);
+        tri.describe();
+        assertThat(outputStream.toString()).as(value).isEqualTo("isosceles");
     }
 
-
-    @Test
-    public void inputNotNumber() {
-        setInputNumber("5 5 R");
-        try {
-            new TriangleImpl();
-            fail("Test must be fail with error " + InputMismatchException.class);
-        } catch (InputMismatchException ime) {
-            ime.getMessage();
-        }
-    }
-
-    @Test
-    public void inputNotInteger() {
-        setInputNumber("5 6,3 4");
-        try {
-            new TriangleImpl();
-            fail("Test must be fail with error " + InputMismatchException.class);
-        } catch (InputMismatchException ime) {
-            ime.getMessage();
-        }
-    }
-
-    @Test
-    public void inputNotNegative() {
-        setInputNumber("-9 7 10");
-        try {
-            new TriangleImpl();
-            fail("Test must fail with exception " + IllegalArgumentException.class);
-        } catch (IllegalArgumentException ime) {
-            assertEquals(ime.getMessage(), "Number must be positive");
-        }
-    }
-
-    @Test
-    public void invalidInputNumber() {
-        setInputNumber("1 1 6");
-        try {
-            new TriangleImpl();
-            fail("Test must be fail with exception " + IllegalArgumentException.class);
-        } catch (IllegalArgumentException ime) {
-            assertEquals(ime.getMessage(), "Number of sides cannot create triangle");
-        }
-    }
-
-
+    @Rule public InfoAboutTest iat = new InfoAboutTest();
+    @Tag("TestTagged")
     @Test
     public void isScalene(){
-        setInputNumber("3 5 4");
-        TriangleImpl tri = new TriangleImpl();
+        TriangleImpl tri = createTriangle("3 5 4");
         tri.describe();
-        assertEquals("scalene", outputStream.toString());
+        assertThat(outputStream.toString()).isEqualTo("scalene");
     }
 
-    @Test
-    public void isIsosceles(){
-        setInputNumber("3 5 5");
-        TriangleImpl tri = new TriangleImpl();
-        tri.describe();
-        assertEquals("isosceles", outputStream.toString());
-    }
-
+    @Tag("TestTagged")
     @Test
     public void isEquilateral(){
-        setInputNumber("5 5 5");
-        TriangleImpl tri = new TriangleImpl();
+        TriangleImpl tri = createTriangle("5 5 5");
         tri.describe();
-        assertEquals("equilateral", outputStream.toString());
+        assertThat(outputStream.toString()).isEqualTo("equilateral");
     }
 
     @AfterEach
@@ -104,6 +60,12 @@ public class testTriangle{
     private void setInputNumber(String number) {
         InputStream is = new ByteArrayInputStream(number.getBytes());
         System.setIn(is);
+    }
+
+
+    private TriangleImpl createTriangle(String s) {
+        setInputNumber(s);
+        return new TriangleImpl();
     }
 
 }
